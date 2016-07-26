f794307d2ab838562c5801b58c91e4c391480ea6
AfroMonkey <moisalejandro@gmail.com>
2016-07-26 11:28:59 -0500

    [IMP] Model fields

--- models/farm.py
+++ models/farm.py
@@ -3,3 +3,10 @@ from openerp import api, fields, models
 
 class Farm(models.Model):
     _name = 'farm'
+
+    id = fields.Char()
+    folio = fields.Char(size=12)
+    owner = fields.Many2one('res.partner')
+    city = fields.Many2one('res.country.state.city')
+    coords = fields.Char()
+    area = fields.Float()
