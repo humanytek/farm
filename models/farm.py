0d3e729672458aa9509bc61d17196c6fbb589cb7
AfroMonkey <moisalejandro@gmail.com>
2016-07-26 12:13:42 -0500

    [IMP] Set fields required

--- models/farm.py
+++ models/farm.py
@@ -4,9 +4,9 @@ from openerp import api, fields, models
 class Farm(models.Model):
     _name = 'farm'
 
-    id = fields.Char()
-    folio = fields.Char(size=12)
-    owner = fields.Many2one('res.partner')
-    city = fields.Many2one('res.country.state.city')
-    coords = fields.Char()
-    area = fields.Float()
+    id_farm = fields.Char(required=True)
+    folio = fields.Char(size=12, required=True)
+    owner = fields.Many2one('res.partner', required=True)
+    city = fields.Many2one('res.country.state.city', required=True)
+    coords = fields.Char(required=True)
+    area = fields.Float(required=True)
