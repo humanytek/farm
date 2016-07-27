9e6bd16757381e3bdd01c3afe87fbd9a8f8cfe21
AfroMonkey <moisalejandro@gmail.com>
2016-07-27 10:40:00 -0500

    [IMP] Set name of farm

--- models/farm.py
+++ models/farm.py
@@ -1,12 +1,18 @@
-from openerp import api, fields, models
+from openerp import api, fields, models, _
 
 
 class Farm(models.Model):
     _name = 'farm'
 
-    id_farm = fields.Char(required=True)
+    name = fields.Char(string="Farm ID", required=True)
     folio = fields.Char(size=12, required=True)
     owner = fields.Many2one('res.partner', required=True)
     city = fields.Many2one('res.country.state.city', required=True)
     coords = fields.Char(required=True)
     area = fields.Float(required=True)
+
+    _sql_constraints = [
+        ('farm_id_unique',
+         'UNIQUE(name)',
+         _("The farm id must be unique")),
+    ]
