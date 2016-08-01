76630ec85b68d6cb12c89d966da572ea9f5a9005
AfroMonkey <moisalejandro@gmail.com>
2016-08-01 12:10:55 -0500

    [IMP] Change farm_id Change farm_id(Many2one) to farm_ids (Many2many)

--- models/purchase_farm.py
+++ models/purchase_farm.py
@@ -4,12 +4,7 @@ from openerp import api, fields, models
 class Farm(models.Model):
     _inherit = 'purchase.order'
 
-    farm_id = fields.Many2one('farm')
-    folio = fields.Char(related="farm_id.folio", readonly=True)
-    farm_owner = fields.Many2one('res.partner', related="farm_id.farm_owner", readonly=True)
-    city = fields.Many2one('res.country.state.city', related="farm_id.city", readonly=True)
-    coords = fields.Char(related="farm_id.coords", readonly=True)
-    area = fields.Float(related="farm_id.area", readonly=True)
+    farm_ids = fields.Many2many('farm')
 
     hired_area = fields.Float()
     expected_performance = fields.Float(default=12)
