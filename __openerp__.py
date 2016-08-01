d72508a8d3bc43ab4ce01e851f280c55aa0bfda9
AfroMonkey <moisalejandro@gmail.com>
2016-08-01 09:47:03 -0500

    [IMP] Add security

--- __openerp__.py
+++ __openerp__.py
@@ -5,7 +5,7 @@
     'website': 'http://humanytek.com',
     'depends': ['purchase', 'purchase_contract_type'],
     'data': [
-        # 'security/ir.model.access.csv',
+        'security/ir.model.access.csv',
         # 'security/farm_access_rules.xml',
         'views/farm.xml',
         'views/farm_purchase.xml',
