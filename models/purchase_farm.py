3941e058e6cad99f8de726456a9037ec4c1ed3c8
AfroMonkey <moisalejandro@gmail.com>
2016-08-02 09:07:29 -0500

    [IMP] Farm areas One2many The farm areas are One2many field

--- models/purchase_farm.py
+++ models/purchase_farm.py
@@ -1,11 +1,11 @@
 from openerp import api, fields, models
 
 
-class Farm(models.Model):
-    _inherit = 'purchase.order'
+class FarmArea(models.Model):
+    _name = 'farm.area'
 
-    farm_ids = fields.Many2many('farm')
-
+    purchase_order_id = fields.Many2one('purchase.order')
+    farm_id = fields.Many2one('farm')
     hired_area = fields.Float()
     expected_performance = fields.Float(default=12)
     tons_to_validate = fields.Float(compute="_get_tons_to_validate", store=False)
@@ -25,3 +25,8 @@ class Farm(models.Model):
     @api.depends('hired_area', 'expected_performance')
     def _get_tons_to_validate(self):
         self.tons_to_validate = self.hired_area * self.expected_performance
+
+class FarmInherit(models.Model):
+    _inherit = 'purchase.order'
+
+    farm_area_ids = fields.One2many('farm.area', 'purchase_order_id')
