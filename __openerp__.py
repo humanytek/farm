347332544c3c7749cc9fcc5469ca37e448391f9d
AfroMonkey <moisalejandro@gmail.com>
2016-08-17 15:56:52 -0500

    [IMP] Security

--- __openerp__.py
+++ __openerp__.py
@@ -6,7 +6,6 @@
     'depends': ['purchase', 'purchase_contract_type'],
     'data': [
         'security/ir.model.access.csv',
-        # 'security/farm_access_rules.xml',
         'views/farm.xml',
         'views/farm_purchase.xml',
     ]
